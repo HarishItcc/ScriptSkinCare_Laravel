5e72d7d7514a5ad9d551c61942c5a2e5be92690f
HarishItcc <44562934+HarishItcc@users.noreply.github.com>
2018-10-30 12:03:25 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Role;
+use App\User;
+use App\RoleUser;
+use App\Supplier;
+use Session;
 
 class LoginController extends Controller
 {
@@ -35,6 +40,28 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectPath()
+    {
+
+         $check_data=RoleUser::join('roles','role_user.role_id','=','roles.id')->Where('role_user.user_id',\Auth::user()->id)->select('roles.*')->first();
+
+         Session::put('role_user_name', $check_data->name);
+         
+         if ($check_data->name=='supplier_admin') {
+
+             $data=Supplier::join('company_details','supplier_details.company_id','=','company_details.id')
+            ->Where('supplier_details.user_id',\Auth::user()->id)
+            ->select('company_details.business_name')->first();
+
+            Session::put('company_name', $data->business_name);
+
+            return route('supplierhome');
+            // or return route('routename');
+        }
+
+        return "/home";
     }
     /*
     public function showLoginForm()
